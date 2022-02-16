871474d6c7ecf65037e374c11780d219e36d3dc0
Austin Baik <baik.austin@gmail.com>
2022-02-15 16:34:10 -0800

    commit tree 28cc073f2449ba48c1379bf2ccf6984491bd2d5f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+// write a function, submitData, that takes two strings as arguments, one representing a user's name
+// and the other representing a user's email.
+
+
+
+// The destination URL
+// Headers for 'Content-Type' and 'Accept', both set to 'application/json'
+// A body with the name and email passed in as arguments to submitData. These should be assigned to name and email keys within an object. This object should then be stringified.
+
+
+
+function submitData(userName, userEmail) {
+    console.log(userName)
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.append(object.id)
+        })
+        .catch(function (message) {
+            document.body.append(message)
+        });
+}
+
